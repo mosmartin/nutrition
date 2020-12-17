bb7b7d7dc919c42f17eb6629cd838d5f6e8c2426
Martin Macharia <mosmartin5@gmail.com>
2020-12-17 22:11:47 +0100

    Create index.ts
    Prepare the package by exporting the types and classes.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Food from './Food';
+import Nutrition from './types/Nutrition';
+import Units from './types/Units';
+import EmptyFoodNameError from './errors/EmptyFoodNameError';
+import InvalidFoodAmountError from './errors/InvalidFoodAmountError';
+
+export { Food, Nutrition, Units, EmptyFoodNameError, InvalidFoodAmountError };
